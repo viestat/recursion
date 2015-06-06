4696625953b590df3e3b71ac1e7baabd8e57d58c
Andrés C. Viesca Ruiz <viestat@icloud.com>
2015-06-05 19:26:04 -0500

    Completed stringifyJSON.

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -5,4 +5,44 @@
 
 var stringifyJSON = function(obj) {
   // your code goes here
+  if (Array.isArray(obj)) {
+  	var i = 0;
+  	var res = "[";
+
+  	while (i < obj.length){
+
+  		if (i !== obj.length - 1){
+  			res += stringifyJSON(obj[i]) + ",";
+  		} else {
+  			res += stringifyJSON(obj[i]);
+  		}
+  		i++;
+  	}
+
+  	return res + "]";
+
+  } else if (typeof(obj) === "object" && obj !== null) {
+  	var res = [];
+  	for (var key in obj){
+  		if (obj[key] !== undefined && typeof(obj[key]) !== "function"){
+  			res.push(stringifyJSON(key) + ":" + stringifyJSON(obj[key]));
+  		}
+  	}
+  	return "{" + res.join(",") + "}";
+
+  } else if (obj === null) {
+  	return "null";
+
+  } else if (typeof(obj) === "undefined") {
+  	return undefined;
+
+  } else if (typeof(obj) === "function") {
+  	return undefined;
+
+  } else if (typeof(obj) === "string"){
+  	return "\"" + obj + "\"";
+
+  } else if (typeof(obj) !== "string") {
+  	return obj + "";
+  }
 };
